92c70d0960591efbb7e60ae81b30aabde506907a
juanitzibarrientos-ship-it <juanitzibarrientos@gmail.com>
2026-02-13 20:10:50 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+// Floating Hearts
+function createHeart() {
+    const heart = document.createElement("div");
+    heart.classList.add("heart");
+    heart.innerHTML = "💖";
+    heart.style.left = Math.random() * 100 + "vw";
+    heart.style.fontSize = (Math.random() * 20 + 15) + "px";
+    document.body.appendChild(heart);
+
+    setTimeout(() => {
+        heart.remove();
+    }, 5000);
+}
+setInterval(createHeart, 300);
